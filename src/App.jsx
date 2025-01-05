c1a750fe725d6e40866cc8b398d7469a60cb30b5
sagun basnet <basnetsagun8@gmail.com>
2025-01-05 14:04:16 +0545

    useState hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // import Form from "./components/form/Form"
+import { useState } from "react"
 import Card from "./components/Card"
 import Login from "./components/form/Login"
 import Register from "./components/form/Register"
@@ -6,9 +7,26 @@ import Navbar from "./components/global/Navbar"
 import Product from "./components/Product"
 const App = () => {
 
+  const [count, setCount] = useState(true);
+
+  // let count = 0;
+
+  const handleClick = () => {
+    setCount(!count);
+    count ? document.body.style.backgroundColor = "black" : document.body.style.backgroundColor = "white"
+    console.log(count);
+  }
+
   return (
     <>
-      <Product />
+      {/* <Navbar /> */}
+
+      {/* <Product /> */}
+
+      <div className="w-full h-[100vh] flex items-center gap-4 justify-center">
+        <button onClick={handleClick} className="p-2 px-4 bg-blue-500 rounded-md">{count ? "ON" : "OFF"}</button>
+      </div>
+
     </>
   )
 }
